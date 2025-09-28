3dd0c5b0c7871d4b61fe0c030f398d7822afcc29
Aakash8546 <aakashsrivastava2151@gmail.com>
2025-09-28 20:25:26 +0530

    h

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+    const titleInput = document.getElementById('titleInput');
+    const durationInput = document.getElementById('durationInput');
+    const dateInput = document.getElementById('dateInput');
+    const prioritySelect = document.getElementById('prioritySelect');
+    const addBtn = document.getElementById('addBtn');
+    const listBox = document.getElementById('listBox');
+    const startAllBtn = document.getElementById('startAll');
+    const endBtn = document.getElementById('endBtn');
+    const finishBtn = document.getElementById('finishBtn');
+    const skipBtn = document.getElementById('skipBtn');
+    const timerBox = document.getElementById('timerBox');
+    const summaryBox = document.getElementById('summaryBox');
+    const summaryItems = document.getElementById('summaryItems');
+    const backBtn = document.getElementById('backBtn');
+    const breakNote = document.getElementById('breakNote');
+    const taskCount = document.getElementById('taskCount');
+    const totalCount = document.getElementById('totalCount');
+    const doingCount = document.getElementById('doingCount');
+    const doneCount = document.getElementById('doneCount');
+
+  
